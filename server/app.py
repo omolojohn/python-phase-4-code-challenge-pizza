62722250e437db2fd2fa49c6ca91b7da7597c260
omolo-j <omoloj135@gmail.com>
2024-07-05 16:34:13 +0300

    updates

--- server/app.py
+++ server/app.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
-from models import db, Restaurant, RestaurantPizza, Pizza
-from flask_migrate import Migrate
+import os
 from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
-import os
+from flask_sqlalchemy import SQLAlchemy
+from models import Restaurant, RestaurantPizza, Pizza  # Assuming models are defined in 'models.py'
+from flask_migrate import Migrate
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
+DATABASE_URI = os.getenv("DATABASE_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.json.compact = False
 
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-db.init_app(app)
 api = Api(app)
 
 @app.route("/")
@@ -28,13 +28,13 @@ class Restaurants(Resource):
 
 class RestaurantDetail(Resource):
     def get(self, id):
-        restaurant = db.session.get(Restaurant, id)
+        restaurant = Restaurant.query.get(id)
         if restaurant:
             return restaurant.to_dict()
         return {'error': 'Restaurant not found'}, 404
 
     def delete(self, id):
-        restaurant = db.session.get(Restaurant, id)
+        restaurant = Restaurant.query.get(id)
         if restaurant:
             db.session.delete(restaurant)
             db.session.commit()
